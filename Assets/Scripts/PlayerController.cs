b4ae904076190da89cee61181577e87855ebb710
Benjamin Xie <benxie7@gmail.com>
2023-11-29 14:17:18 -0800

    Added Sprint
    Left-Shift to sprint

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,22 +7,32 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    #region Variables: Movement
     private Vector2 _input;
     private CharacterController _characterController;
     private Vector3 _direction;
+    
+    [SerializeField] private float speed;
 
+    [SerializeField] private Movement movement;
+    #endregion
+    #region Variables: Rotation
     [SerializeField] private float rotationSpeed = 500f;
     private Camera _mainCamera;
 
-    [SerializeField] private float speed;
+    #endregion
+    #region Variables: Gravity
 
     private float _gravity = -9.81f;
     [SerializeField] private float gravityMultiplier = 3.0f;
     private float _velocity;
 
+    #endregion
+    #region Variables: Jumping
     [SerializeField] private float jumpPower;
     private int _numberOfJumps;
     [SerializeField] private int maxNumberOfJumps = 2;
+    #endregion
 
 
     private void Awake()
@@ -54,18 +65,21 @@ public class PlayerController : MonoBehaviour
         if (_input.sqrMagnitude == 0) return;
         _direction = Quaternion.Euler(0.0f, _mainCamera.transform.eulerAngles.y, 0.0f) * new Vector3(_input.x, 0.0f, _input.y);
         var targetRotation = Quaternion.LookRotation(_direction, Vector3.up);
+        
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
     private void ApplyMovement()
     {
-        _characterController.Move(_direction * speed * Time.deltaTime);
+        var targetSpeed = movement.isSprinting ? movement.speed * movement.multiplier : movement.speed;
+        movement.currentSpeed = Mathf.MoveTowards(movement.currentSpeed, targetSpeed, movement.acceleration * Time.deltaTime);
+
+        _characterController.Move(_direction * movement.currentSpeed * Time.deltaTime);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<Vector2>();
-        _direction = new Vector3(x:_input.x, y:0.0f, z:_input.y);
-        Debug.Log(_input);
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
     }
 
     public void Jump(InputAction.CallbackContext context) 
@@ -78,6 +92,12 @@ public class PlayerController : MonoBehaviour
         _velocity = jumpPower;
     }
 
+    public void Sprint(InputAction.CallbackContext context)
+    {
+        movement.isSprinting = context.started || context.performed;
+    }
+
+
     private IEnumerator WaitForLanding()
     {
         yield return new WaitUntil(() => !IsGrounded());
@@ -87,4 +107,15 @@ public class PlayerController : MonoBehaviour
     }
 
     private bool IsGrounded() => _characterController.isGrounded;
+}
+
+[Serializable]
+public struct Movement
+{
+    public float speed;
+    public float multiplier;
+    public float acceleration;
+
+    [HideInInspector] public bool isSprinting;
+    [HideInInspector] public float currentSpeed;
 }
